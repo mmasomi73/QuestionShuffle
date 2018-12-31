d02236640cc1e35ef030fc46c02fa8b84631bad4
mma <mmasomi73@gmail.com>
2018-12-31 14:58:10 +0330

    put layouts view install packages

---
+++ resources/views/admin/partials/right-side.blade.php
@@ -0,0 +1,170 @@
+<nav class="pcoded-navbar">
+    <div class="pcoded-inner-navbar main-menu">
+        <div class="">
+            <div class="main-menu-header">
+                <img class="img-menu-user img-radius" src="{{url('/')}}/assets/images/avatar-4.jpg" alt="User-Profile-Image">
+                <div class="user-details">
+                    <p id="more-details">
+                        <i class="feather icon-chevron-down m-r-10"></i>
+                        صابر مرندی
+                    </p>
+                </div>
+            </div>
+            <div class="main-menu-content">
+                <ul>
+                    <li class="more-details">
+                        <a href="#">
+                            <i class="feather icon-user"></i>پروفایل
+                        </a>
+                        <a href="#!">
+                            <i class="feather icon-settings"></i>تنظمیات
+                        </a>
+                        <a href="auth-normal-sign-in.html">
+                            <i class="feather icon-log-out"></i>خروج
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="pcoded-navigation-label">لایه ها</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="active">
+                <a href="javascript:void(0)" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+                    <span class="pcoded-mtext">داشبورد</span>
+                </a>
+            </li>
+            <li class="pcoded-hasmenu">
+                <a href="javascript:void(0)" class="waves-effect waves-dark">
+                    <span class="pcoded-micon"><i class="feather icon-box"></i></span>
+                    <span class="pcoded-mtext">محتواها</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="">
+                        <a href="breadcrumb.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">نمرات</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="button.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">درس ها</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="accordion.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">محتواها</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="tabs.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">تکالیف</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="color.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">رنگ ها</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="pcoded-navigation-label">وظایف </div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class=" ">
+                <a href="form-elements-component.html" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-clipboard"></i>
+                                    </span>
+                    <span class="pcoded-mtext">فرم ها</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="bs-basic-table.html" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-credit-card"></i>
+                                    </span>
+                    <span class="pcoded-mtext">جداول</span>
+                </a>
+            </li>
+        </ul>
+        <div class="pcoded-navigation-label">چارت ها</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="">
+                <a href="chart-morris.html" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-pie-chart"></i>
+                                    </span>
+                    <span class="pcoded-mtext">نمودارها</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="map-google.html" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-map"></i>
+                                    </span>
+                    <span class="pcoded-mtext">نقشه ها</span>
+                </a>
+            </li>
+            <li class="pcoded-hasmenu ">
+                <a href="javascript:void(0)" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-unlock"></i>
+                                    </span>
+                    <span class="pcoded-mtext">صفحات</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="">
+                        <a href="auth-normal-sign-in.html" target="_blank" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">ورود</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="auth-sign-up.html" target="_blank" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">ثبت نام</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="sample-page.html" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">ساده</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <div class="pcoded-navigation-label">دیگر</div>
+        <ul class="pcoded-item pcoded-left-item">
+            <li class="pcoded-hasmenu ">
+                <a href="javascript:void(0)" class="waves-effect waves-dark">
+                                    <span class="pcoded-micon">
+                                        <i class="feather icon-list"></i>
+                                    </span>
+                    <span class="pcoded-mtext">ردیف</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class="">
+                        <a href="javascript:void(0)" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">ردیف 2</span>
+                        </a>
+                    </li>
+                    <li class="pcoded-hasmenu ">
+                        <a href="javascript:void(0)" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">زیر ردیف 2.3</span>
+                        </a>
+                        <ul class="pcoded-submenu">
+                            <li class="">
+                                <a href="javascript:void(0)" class="waves-effect waves-dark">
+                                    <span class="pcoded-mtext">ردیف 3</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="">
+                        <a href="javascript:void(0)" class="waves-effect waves-dark">
+                            <span class="pcoded-mtext">Menu Level 2.3</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</nav>
